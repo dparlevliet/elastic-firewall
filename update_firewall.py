4ff273cc558dcf7119e13f71eead475fedb23d15
David Parlevliet <dave@leaguecraft.com>
2013-03-16 12:55:15 +1100

    capture KeyError + Patch for pid check/delete

--- update_firewall.py
+++ update_firewall.py
@@ -55,7 +55,7 @@ def main():
   pid_path = '/var/run/elastic-firewall-update.pid'
   try:
     pid = open(pid_path).read()
-    print "Elastic firewall is already running an update: %d" % pid
+    print "Elastic firewall is already running an update: %s" % pid
     sys.exit(1)
   except:
     open(pid_path, 'w').write(os.getpid())
@@ -67,15 +67,18 @@ def main():
   # I hate exec. Keep an eye out for better solutions to this
   exec "from api.%s import Api" % config['server_group']
 
-  if 'block_all' in config and config['block_all'] == True \
-      and 'block_all_assigned' not in rules.rules:
-    ipt.block_all()
-    rules.rules['block_all_assigned'] = True
-    del rules.rules['allow_all_assigned']
-  elif 'allow_all_assigned' not in rules.rules:
-    ipt.allow_all()
-    rules.rules['allow_all_assigned'] = True
-    del rules.rules['block_all_assigned']
+  try:
+    if 'block_all' in config and config['block_all'] == True \
+        and 'block_all_assigned' not in rules.rules:
+      ipt.block_all()
+      rules.rules['block_all_assigned'] = True
+      del rules.rules['allow_all_assigned']
+    elif 'allow_all_assigned' not in rules.rules:
+      ipt.allow_all()
+      rules.rules['allow_all_assigned'] = True
+      del rules.rules['block_all_assigned']
+  except KeyError:
+    pass
 
   api = Api()
   for key in config[config['server_group']]:
@@ -103,6 +106,7 @@ def main():
 
   rules.update_firewall()
   rules.save()
+  os.unlink(pid_path)
 
 
 if __name__ == '__main__':
