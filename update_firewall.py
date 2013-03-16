a21ecefab7fc0071323ecb9597492e59b0532d30
David Parlevliet <dave@leaguecraft.com>
2013-03-16 12:42:03 +1100

    check for existing process && try manage global iptables rules

--- update_firewall.py
+++ update_firewall.py
@@ -52,12 +52,30 @@ class ElasticRules():
 
 
 def main():
+  pid_path = '/var/run/elastic-firewall-update.pid'
+  try:
+    pid = open(pid_path).read()
+    print "Elastic firewall is already running an update: %d" % pid
+    sys.exit(1)
+  except:
+    open(pid_path, 'w').write(os.getpid())
+
   rules = ElasticRules()
   rules.load()
   config = json.loads(open('./config.json').read())
 
   # I hate exec. Keep an eye out for better solutions to this
   exec "from api.%s import Api" % config['server_group']
+
+  if 'block_all' in config and config['block_all'] == True \
+      and 'block_all_assigned' not in rules.rules:
+    ipt.block_all()
+    rules.rules['block_all_assigned'] = True
+    del rules.rules['allow_all_assigned']
+  elif 'allow_all_assigned' not in rules.rules:
+    ipt.allow_all()
+    rules.rules['allow_all_assigned'] = True
+    del rules.rules['block_all_assigned']
 
   api = Api()
   for key in config[config['server_group']]:
